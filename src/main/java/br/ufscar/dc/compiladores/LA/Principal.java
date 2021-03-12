b25c18028c852095a8b08c00a0de2af0f90511f7
FelipeAC98 <felipe.alvcor@gmail.com>
2021-03-11 23:37:19 -0300

    Rodando o parser no arquivo de entrada
    Erros ainda nao sao tratados, aparentemente vai precisar criar algum
    tipo de erroListener. https://stackoverflow.com/questions/21661899/get-all-antlr-parsing-errors-as-list-of-string

--- src/main/java/br/ufscar/dc/compiladores/LA/Principal.java
+++ src/main/java/br/ufscar/dc/compiladores/LA/Principal.java
@@ -9,7 +9,7 @@
 /*                                                                               */ 
 /*                  ALUNOS: Felipe Alves        RA: 744335                       */
 /*                          Karolayne Arrais    RA: 726460                       */	
-/*                          Mariana Zagatti     RA:	628620                   */
+/*                          Mariana Zagatti     RA: 628620                   */
 /*                                                                               */
 /*  Instrucoes para executar o programa:                                         */
 /*      apos clicar em Build, abra o terminal do windows no                      */
@@ -29,45 +29,28 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Token;
 
-/**
- *
- * @author felip
- */
 public class Principal {
     public static void main(String args[]) throws IOException{
         
         CharStream cs= CharStreams.fromFileName(args[0]);       //utilizado para leitura do arquivo passado como entrada contendo codigo com programa em linguagem algoritmica 
-        LA lex = new LA(cs); 
+        LALexer lex = new LALexer(cs);
         FileOutputStream saida = new FileOutputStream(args[1]); //utilizado para escrita no arquivo de saida apos analisa lexica
         Token aux = null;   //token auxiliar para analise
-       
+        CommonTokenStream tokens = new CommonTokenStream(lex);
+        LAParser parser = new LAParser(tokens);
+        parser.programa();
+        
+        //Verificando erros lexicos aqui
         while((aux = lex.nextToken()).getType() != Token.EOF){  //enquanto tivermos caracteres no arquivo de entrada para serem analisados 
-            //System.out.print("<"+LALexer.VOCABULARY.getDisplayName(aux.getType())+","+aux.getText()+">");
-            
+
             //para formacao da parte direita do token
             String direita_token = "'" + aux.getText() + "'"; // usada para montagem do token <getText(),direita_token>
-            
-            // verifica se foi identificado algum identificador, cadeia, numero inteiro ou numero real definido pela gramatica
-            switch (LA.VOCABULARY.getDisplayName(aux.getType())) {
-                case "IDENT":
-                    direita_token = LA.VOCABULARY.getDisplayName(aux.getType());       // forma token <'aux.getText()','IDENT'>
-                    break;
-                case "CADEIA":
-                    direita_token = LA.VOCABULARY.getDisplayName(aux.getType());       // forma token <'aux.getText()','CADEIA'>
-                    break;
-                case "NUM_INT":
-                    direita_token = LA.VOCABULARY.getDisplayName(aux.getType());       // forma token <'aux.getText()','NUM_INT'>
-                    break;
-                case "NUM_REAL":
-                    direita_token = LA.VOCABULARY.getDisplayName(aux.getType());       // forma token <'aux.getText()','NUM_REAL'>
-                    break;
-            }
-                   
-        
-            // verifica erros lexicos definidos na gramatica
-            switch (LA.VOCABULARY.getDisplayName(aux.getType())) {
+           
+            // verifica erros lexicos definidos na gramatica - trabalho 1 - ainda eh preciso printar o erro lexico
+            switch (LALexer.VOCABULARY.getDisplayName(aux.getType())) {
                 case "ERRO_SIMBOLO":
                     saida.write(("Linha " + aux.getLine() + ": " + aux.getText() + " - simbolo nao identificado\n").getBytes());    //imprime a linha de erro e o tipo de erro
                     saida.close();
@@ -80,10 +63,13 @@ public class Principal {
                     saida.write(("Linha " + aux.getLine() + ": cadeia literal nao fechada\n").getBytes());    //imprime a linha de erro e o tipo de erro
                     saida.close();
                     return;
-                default:
-                    saida.write(("<'" + aux.getText() + "'," + direita_token+ ">\n").getBytes()); //forma token padrao das palavras chaves e simbolos <'aux.getText()','aux.getText()'>
             }
         }
+        
+        //parte para retornar ao comeco do arquivo e tratar erro sintatico
+        
+        //Fecha o arquivo
         saida.close();
+        
     }
 }
